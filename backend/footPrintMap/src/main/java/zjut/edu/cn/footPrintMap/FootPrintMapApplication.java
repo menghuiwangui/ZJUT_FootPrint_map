92ba4497b57b336febaa327813729daf15289c2b
menghuiwangui <2964668101@qq.com>
2026-06-30 00:36:19 +0800

    1.0.2
    前后端框架初始化

---
+++ backend/footPrintMap/src/main/java/zjut/edu/cn/footPrintMap/FootPrintMapApplication.java
@@ -0,0 +1,13 @@
+package zjut.edu.cn.footPrintMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootPrintMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootPrintMapApplication.class, args);
+	}
+
+}
---
+++ backend/footPrintMap/src/test/java/zjut/edu/cn/footPrintMap/FootPrintMapApplicationTests.java
@@ -0,0 +1,13 @@
+package zjut.edu.cn.footPrintMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootPrintMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
